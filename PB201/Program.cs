bee54e18e7b3cf61ba16821f8a04f86d11968861
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-05-17 16:24:36 +0400

    Add files via upload

---
+++ PB201/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Pb201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string sentence = "Hello World";
+                PrintInitials(sentence);
+            }
+
+            static void PrintInitials(string input)
+            {
+                string[] words = input.Split(' ');
+
+                foreach (string word in words)
+                {
+                    if (!string.IsNullOrEmpty(word))
+                    {
+                        Console.Write(word[0] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
+    
