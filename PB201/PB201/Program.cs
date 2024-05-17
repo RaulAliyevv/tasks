a9dec24cb49f94d3db330a7ff8a5cfbda4e3c8f3
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-05-17 15:57:25 +0400

    Add files via upload

---
+++ PB201/PB201/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PB201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10; 
+            PrintSumOfEvenNumbers(number);
+        }
+        static void PrintSumOfEvenNumbers(int n) 
+
+        { int sum = 0;
+            for (int i = 1; i <= n; i++) 
+            { 
+                if (i % 2 == 0) 
+                { 
+                    sum += i; }
+            } 
+            Console.WriteLine("sum number: " + sum); }
+    }
+}    
+    
