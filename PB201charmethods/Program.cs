5e2c453f628acddb69fa802ca4ed46afdb096f8b
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-05-20 23:40:54 +0400

    Add files via upload

---
+++ PB201charmethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PB201CharMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "a 2 4 e $ 5 y *";
+
+            Console.WriteLine(password.Length);
+
+            Console.WriteLine(password.Contains("  "));
+
+        }
+    }
+}
