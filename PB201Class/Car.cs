267c1e7798381125f39ddc692a32f1decbab8d1d
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-05-21 21:55:54 +0400

    Add files via upload

---
+++ PB201Class/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB201Class
+{
+    public class Car
+    {
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public string Model;
+        public string Brand;
+
+
+        public void Drive(int km) 
+        {
+            Console.WriteLine(CurrentFuel = CurrentFuel - (FuelFor1Km * km));
+        }
+
+        public void ShowFullData() 
+        {
+            Console.WriteLine($"{Brand} - {Model} - {CurrentFuel} - {FuelFor1Km}");
+        }
+
+        public string GetFullData() 
+        {
+            return $"{Brand} - {Model} - {CurrentFuel} - {FuelFor1Km}";
+        }
+
+
+    }
+}
---
+++ PB201Class/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PB201Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Car car = new Car();
+            car.CurrentFuel = 80;
+            car.FuelFor1Km = 4;
+            car.Model = "MercedesBenz";
+            car.Brand = "2023";
+
+            car.Drive(5);
+
+
+            car.ShowFullData();
+
+
+            
+        }
+    }
+}
