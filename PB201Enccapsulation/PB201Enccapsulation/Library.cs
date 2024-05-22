4095c68895e5d6d27dd4759fa15c60506aead7af
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-05-23 00:42:51 +0400

    Add files via upload

---
+++ PB201Enccapsulation/PB201Enccapsulation/Library.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB201Enccapsulation
+{
+    public class Library
+    {
+        public Book[] Books = new Book[0];
+
+        public void AddBook(Book book) 
+        {
+            Array.Resize(ref Books, Books.Length + 1);
+            Books[Books.Length-1] = book;
+        }
+
+        public Book[] GetFilteredBooks(string genreName) 
+        {
+            Book[] filteredBooks = Array.Empty<Book>();
+
+            foreach (var book in Books) 
+            {
+                if (book.Genre.ToLower() == genreName.ToLower()) 
+                {
+                    Array.Resize(ref filteredBooks, filteredBooks.Length + 1);
+                    filteredBooks[filteredBooks.Length -1] = book;
+
+                }
+            }
+
+            return filteredBooks;
+
+        }
+    }
+}
---
+++ PB201Enccapsulation/PB201Enccapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace PB201Enccapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book1 = new Book (1, "Vuqar Bileceri", 20, "Lyrics");
+           Book book2 = new Book(2, "Stephen King", 11.90, "Horror");
+           Book book3 = new Book(3, "Sherlock Holmes", 35, "Detective");
+           Book book4 = new Book(4, "The Detective Novel", 40.20, "Detective");
+
+            // Console.WriteLine(book1.Genre);
+
+            Library pb201 = new Library();
+
+            pb201.AddBook (book1);
+            pb201.AddBook (book2);
+            pb201.AddBook (book3);
+            pb201.AddBook(book4);
+
+            foreach (var book in pb201.GetFilteredBooks("horror")) 
+            {
+                Console.WriteLine (book.Name + " " + book.Genre);
+            }
+            
+
+
+
+
+
+        }
+    }
+}
