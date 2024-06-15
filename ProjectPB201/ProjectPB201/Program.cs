0f6e48ab1c8ba74da405ed39c618dca3d6c880b6
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-06-15 14:23:14 +0400

    Add files via upload

---
+++ ProjectPB201/ProjectPB201/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace ProjectPB201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Daraqın maksimum güllə tutumunu daxil edin: ");
+            int maxBulletCapacity = Convert.ToInt32(Console.ReadLine());
+
+            Weapon weapon = new Weapon(maxBulletCapacity);
+
+            int choice;
+            do
+            {
+                Console.WriteLine("\n0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Shoot metodu üçün");
+                Console.WriteLine("2 - Fire metodu üçün");
+                Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4 - Reload metodu üçün");
+                Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                Console.WriteLine("6 - Proqramdan dayandırmaq üçün");
+                Console.WriteLine("7 - Edit: 1 - Tutumu dəyişsin, 2 - Güllə sayı dəyişsin");
+
+                Console.Write("Seçim edin (0-7): ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine($"Silahın qalıq güllə sayı: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine($"Silahın qalıq güllə sayı: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 7:
+                        Console.Write("Seçim edin (1 - Tutumu dəyişsin, 2 - Güllə sayı dəyişsin): ");
+                        int editChoice = Convert.ToInt32(Console.ReadLine());
+                        if (editChoice == 1)
+                        {
+                            Console.Write("Yeni maksimum güllə tutumunu daxil edin: ");
+                            int newMaxBulletCapacity = Convert.ToInt32(Console.ReadLine());
+                            weapon = new Weapon(newMaxBulletCapacity);
+                        }
+                        else if (editChoice == 2)
+                        {
+                            Console.Write("Yeni güllə sayını daxil edin: ");
+                            int newBulletCount = Convert.ToInt32(Console.ReadLine());
+                            weapon = new Weapon(maxBulletCapacity);
+                            weapon.Reload(newBulletCount);
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("Proqram dayandırıldı.");
+                        break;
+                    default:
+                        Console.WriteLine("Yanlış seçim! Xahiş edirəm yenidən cəhd edin.");
+                        break;
+                }
+            } while (choice != 6);
+        }
+    }
+}
---
+++ ProjectPB201/ProjectPB201/Weapon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPB201
+{
+    public class Weapon
+    {
+        private int bulletCount;
+        private int maxBulletCapacity;
+        private FireMode fireMode;
+
+        public Weapon(int maxBulletCapacity)
+        {
+            this.maxBulletCapacity = maxBulletCapacity;
+            this.bulletCount = maxBulletCapacity; 
+            this.fireMode = FireMode.Single; 
+        }
+
+        public void Shoot()
+        {
+            if (bulletCount > 0)
+            {
+                Console.WriteLine("Güllə atıldı!");
+                bulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("Güllə yoxdur!");
+            }
+        }
+
+        public void Fire()
+        {
+            if (bulletCount > 0)
+            {
+                Console.WriteLine($"Bütün güllələr atəşləndi: {bulletCount} güllə");
+                bulletCount = 0;
+            }
+            else
+            {
+                Console.WriteLine("Güllə yoxdur!");
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return bulletCount;
+        }
+
+        public void Reload(int newBulletCount)
+        {
+            bulletCount = maxBulletCapacity;
+            Console.WriteLine($"Silah yenidən dolduruldu. Cəmi {maxBulletCapacity} güllə daxil edildi.");
+        }
+
+        public void ChangeFireMode()
+        {
+            fireMode = fireMode == FireMode.Single ? FireMode.Automatic : FireMode.Single;
+            Console.WriteLine($"Atış modu dəyişdirildi: {fireMode}");
+        }
+
+        internal void Reload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
