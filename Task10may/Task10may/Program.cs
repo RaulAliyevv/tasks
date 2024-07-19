8f77b9918f00b6a3be44383cf96341de82c688fb
RaulAliyevv <169164911+RaulAliyevv@users.noreply.github.com>
2024-07-19 15:34:10 +0400

    Add files via upload

---
+++ Task10may/Task10may/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                if (i < 65)
+                {
+                    Console.WriteLine($"{i}: Kəsildin");
+                }
+                else if (i >= 65 && i < 85)
+                {
+                    Console.WriteLine($"{i}: Adi Diplom");
+                }
+                else if (i >= 85 && i < 95)
+                {
+                    Console.WriteLine($"{i}: Şərəf Diplomu");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}: Yüksək Şərəf");
+                }
+            }
+        }
+    }
+}
